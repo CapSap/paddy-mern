be79b768a9ac0c34d3ed07800047410d3d7bbd9c
CapSap <cshotam@gmail.com>
2021-12-10 22:24:41 +1100

    added eslint and prettier

--- first.test.js
+++ first.test.js
@@ -1,5 +1,13 @@
+const server = require("./src/server");
 
+describe("server", () => {
+  it(" test should run", () => {});
+});
 
-it('should run', () => {
-    
-})
+// express server should serve react app
+
+// express should connect with DB and return some data.
+
+// put data from react to db.
+
+// render data in react from DB
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-// bodyparser middleware
-app.use(bodyParser.json());
-
