6acf38a38d4413b53af37572533f6cae28b46863
CapSap <cshotam@gmail.com>
2021-12-10 21:44:02 +1100

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// bodyparser middleware
+app.use(bodyParser.json());
+
