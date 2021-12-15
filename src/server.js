c6d768e89672b5b93c58023d714a071b46d7364c
CapSap <cshotam@gmail.com>
2021-12-15 18:25:33 +1100

    mongo db connect

--- src/server.js
+++ src/server.js
@@ -10,13 +10,16 @@ app.use(bodyParser.json());
 //DB config
 const db = require("../config/keys").mongoURI;
 
-//connect
-mongoose.connect(db).then(() => {
-  console.log("db connected");
-});
+//connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => console.log(err));
 
-const port = 5000;
+const port = process.env.port || 5000;
 
 if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => console.log("app running"));
+  app.listen(port, () => console.log(`Server started on port ${port}`));
 }
