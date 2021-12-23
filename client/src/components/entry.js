18652322849cb1d05e382a3a3149658b376e1817
CapSap <cshotam@gmail.com>
2021-12-24 07:46:03 +1100

    add some inputs

--- client/src/components/entry.js
+++ client/src/components/entry.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Entry() {
-  return <div>entry comp</div>;
+  function onSubmit(e) {
+    e.preventDefault();
+    for (let i = 0; i < e.target.length - 1; i++) {
+      console.log("button pressed " + e.target[i].value);
+    }
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          onSubmit(e);
+        }}
+      >
+        <label for="order-number">Order Number: </label>
+        <input type="text" id="order-number" value="1234"></input>
+        <label for="customer-name">Customer Name: </label>
+        <input type="text" id="customer-name" value="customer name"></input>
+        <label for="stores">Items Needed:</label>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
 
 export default Entry;
