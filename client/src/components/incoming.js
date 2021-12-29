e331b3fd57baf9e14a6b1fe04ebdf5380c6fb9a8
CapSap <cshotam@gmail.com>
2021-12-29 21:36:19 +1100

    set up incoming component

--- client/src/components/incoming.js
+++ client/src/components/incoming.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Incoming() {
-  return <div>incoming</div>;
+  const [fromDB, setFromDB] = useState("");
+
+  useEffect(() => {
+    fetch("/api/orders", { method: "GET", redirect: "follow" })
+      .then((res) => res.json())
+      .then((data) => setFromDB(data))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return (
+    <div>
+      Orders to be picked up from your store:
+      <ul>
+        {fromDB
+          ? fromDB.map((x) => {
+              return (
+                <li key={x.orderNumber}>
+                  <p>
+                    Order: {x.orderNumber} for {x.customerName}
+                  </p>
+
+                  {x.orderedItems.map((y) => {
+                    return (
+                      <p>
+                        {y.sendingStore} sending {y.item}
+                      </p>
+                    );
+                  })}
+                </li>
+              );
+            })
+          : "Data Loadding"}
+      </ul>
+    </div>
+  );
 }
 
 export default Incoming;
